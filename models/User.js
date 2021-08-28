9e68c238a947dd1ad1cdd00f526618d40f264089
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-08-28 13:58:23 +1000

    added Models for the table structure

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "U001",
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "*******",
+    },
+    active: { type: DataTypes.BOOLEAN, defaultValue: true },
+    email: { type: DataTypes.STRING, allowNull: false },
+    created_dt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_dt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = User;

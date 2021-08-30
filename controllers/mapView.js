6d6f529f737008455b256c93df127f1402a17a7e
untitledevolper <teoman.h.aydogan@gmail.com>
2021-08-30 19:14:40 +1000

    Quick commit

--- controllers/mapView.js
+++ controllers/mapView.js
@@ -1,6 +1,17 @@
 require('dotenv').config()
 const geoCodingApi = require("./api/geoApi");
 
+function geoApi(location) {
+    var api_key = process.env.GOOGLE_API
+    url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${api_key}`;
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data => output = data)
+    .then(() => console.log(output));
+  
+  }
+
 let map;
     
 // Initializes the google map with a strating central location

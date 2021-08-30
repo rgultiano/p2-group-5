94b25b2a06e7351228ee67e49835f6361616d1ee
Ron G <62861315+rgultiano@users.noreply.github.com>
2021-08-30 17:26:47 +1000

    Added login page and handlebars functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.get('/login', (req, res) => {
+  
+    res.render('login');
+  });
 
 module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,38 @@
+const loginFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    // Gather the data from the form elements on the page
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (email && password) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        if(response.status == 401){
+            const {message} = await response.json();
+            // make the password border red
+            document.querySelector('#password').classList.add("border-red-500")
+
+            // display the error message
+            document.querySelector('.login-error-message').innerText = message;
+        } else {
+            // display the error message
+            document.querySelector('.login-error-message').innerText = "Unexpected error! Please try again later.";
+        }
+
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);

051c6d1e629f48945b55b739c62778b69137987c
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-08-31 22:45:56 +1000

    added the routes for signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,10 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.get('/login', (req, res) => {
-  
-    res.render('login');
-  });
-
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
 module.exports = router;

ef092e46950ec7c59e020e11c63b0383dae64f94
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-09-02 18:46:28 +1000

    fixed the indentation as per review comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,9 @@ const apiRoutes = require("./api");
 router.get("/login", (req, res) => {
   res.render("login");
 });
+
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
+
 module.exports = router;

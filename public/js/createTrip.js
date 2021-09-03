01cb215acd9e305e14fc13ad426ec629bde924cd
untitledevolper <teoman.h.aydogan@gmail.com>
2021-09-03 22:57:57 +1000

    Added create trip page with js and custom css

---
+++ public/js/createTrip.js
@@ -0,0 +1,169 @@
+let map;
+// This variable will hold id for each card along with its lat, lng value
+var coor = [];
+// a temp array to hold all coordinates of all listed locations for the polylines to be rendered
+var polyCoor = [];
+
+// Tried using Jquery to allow for 'enter' to register as 'click'. Doesn't work :(
+$("#add").keyup(function(event) {
+  if (event.keyCode === 13) {
+      $("#submit").click();
+  }
+});
+
+function getId(id) {
+  return document.getElementById(id);
+}
+
+function uid() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+}
+
+function appndCoor(id, value) {
+  coor.push({id: id, values: {value}})
+}
+
+function listPolyCoor() {
+  polyCoor = [];
+  for (i in coor) {
+    polyCoor.push(coor[i]["values"]["value"])
+  }
+}
+
+function getSearch() {
+  var input = getId("add");
+  geoCodingApi(input.value);
+}
+
+function addCityCard(id, input) {
+  listContent = getId("tripCreator");
+  listContent.innerHTML += `
+  <div class="card" id="${id}_card">
+    <div class="cardHeader">
+        <button class="deleteBtn" id="${id}_deleteCard" onclick="deleteCard(this)"><i class="far fa-trash-alt"></i></button>
+    </div>  
+    <div class="cardBody">
+        <input class="header3" type="text" name="title" id="${id}_title" value="${input}">
+        <textarea class="tripEntry" name="tripEntry" placeholder="..." cols='35' rows='10' id='${id}_tripEntry'></textarea>
+    </div>     
+  </div>`
+
+}
+
+function deleteCard(btn) {
+  var id = btn.id;
+  id = id.slice(0, -11);
+  $('#'+id+"_card").remove();
+
+  polyCoor = []
+
+  for (i in coor) {
+    if (coor[i]["id"] == id) {
+      coor.splice(i, 1);
+    }
+  }
+  listPolyCoor()
+  reInit()
+}
+
+function geoCodingApi(location) {
+  // Require a dotenv for api key
+  var api_key = "API_KEY_GOES_HERE";
+  url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${api_key}`;
+  
+  fetch(url)
+  .then(response => response.json())
+  .then(data => output = data)
+  .then(() => renderMap(output, location))
+}
+
+function renderMap(output, location) {
+  var latLng = output['results'][0]['geometry']['location'];
+
+  var id = uid();
+  appndCoor(id, latLng);
+
+  initMap(latLng);
+  addCityCard(id, location);
+
+  listPolyCoor();
+  polyline(polyCoor);
+}
+
+function reInit() {
+  // reinit map
+  initMap(polyCoor[0]);
+
+  //reinit polylines
+  polyline(polyCoor);
+}
+
+function initMap(coor) {
+  var mapOptions = {
+    zoom: 12,
+    center: coor,
+    disableDefaultUI: true,
+    mapId: '8a454c346a18cb76'
+  }
+
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+} 
+
+
+function polyline(location) {
+  const path = new google.maps.Polyline({
+        path: location,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+      path.setMap(map);
+}
+
+
+// Saving to database functionality required
+function saveData() {
+  console.log(coor);
+
+  var allCardData = [];
+  var cardTitles = [];
+  var cardEntries = [];
+
+  for (i in coor) {
+    var id = coor[i]["id"]
+    var value = coor[i]["values"]["value"]
+    var title = getId(id+"_title").value
+    var entry = getId(id+"_tripEntry").value
+    allCardData.push({id: id, content: {title: title, entry: entry}, value: {value}})
+  }
+
+  console.log(allCardData)
+  
+  // How the 'allCardData' var appears for each card once saved. Below is all the required data  
+  // {
+  //   id: "id",
+  //   content: {
+  //     title: "title",
+  //     entry: "entry"
+  //   },
+  //   value: {
+  //     value: {lat: 123, lng: 321}
+  //   }
+  // }
+}
+
+
+
+// How 'coor' var is layed out
+// {
+//   id: "id", 
+//   values: {
+//     value: {
+//       lat: 123,
+//       lng: 321
+//     }
+//   }
+// }

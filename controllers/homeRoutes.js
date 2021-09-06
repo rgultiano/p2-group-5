0d149843ab3d42cb98c769897e894feb3ed1f03d
Ron G <62861315+rgultiano@users.noreply.github.com>
2021-09-06 15:21:30 +1000

    Added payment form, no payment functionality yet

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 const { User, Quote, UserAuth, Trip, Destination} = require('../models');
 const { withAuth } = require('../utils/auth');
 
+
+var formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'AUD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 // render homepage as index
 router.get('/', async (req, res) => {
   if (req.session.logged_in) {
@@ -21,8 +29,8 @@ router.get('/', async (req, res) => {
     res.render('home', {
       trips: tripArr.filter(trip=>trip.status == 'open').map(trip=> {return {showDelete: true, showEdit: true, showSendCuration: true, ...trip}}),
       completed_trips: tripArr.filter(trip=>trip.status == 'completed').map(trip=> {return {showDelete: true, showEdit: true, ...trip}}),
-      curated_trips: tripArr.filter(trip=>trip.status == 'awaiting_curation').map(trip=> {return {showDelete: true, showView: true, ...trip}}),
-      booked_trips: tripArr.filter(trip=>trip.status == 'booked').map(trip=> {return {showDelete: true, showView: true, ...trip}}),
+      curated_trips: tripArr.filter(trip=>trip.status == 'awaiting_curation').map(trip=> {return {showDelete: true, showView: true, url: `/trip/${trip.id}/quotes`, ...trip}}),
+      booked_trips: tripArr.filter(trip=>trip.status == 'booked').map(trip=> {return {showDelete: true, showView: true, url: '#', ...trip}}),
     });
     return;
   }
@@ -67,7 +75,6 @@ router.get("/trip/:id/curate", withAuth, async (req, res) => {
   });
   res.locals.trip_id = req.params.id;
   res.locals.curate_view = true;
-  res.locals.trip = 
   console.log(tripData.get({plain:true}));
   res.render("trip", {trip: tripData.get({plain:true})});
 });
@@ -89,6 +96,83 @@ router.get("/trip/:id/quotes", withAuth, async (req, res) => {
 
   console.log(quoteData.map(quote=>{return quote.get({plain:true})}));
   res.render("trip_quotes", {trip_quotes: quoteData.map(quote=>{return quote.get({plain:true})})});
+});
+
+router.get("/trip/:id/quotes/:quote_id", withAuth, async (req, res) => {
+  const tripData = await Trip.findByPk(req.params.id, {
+    include: [{ model: Destination}],
+    where: {
+      user_id: req.session.user_id,
+    },
+    order:[['destinations', 'order', 'ASC']],
+  });
+
+  const quoteData = await Quote.findByPk(req.params.quote_id, 
+    {
+      include: [{ model: Trip, 
+      where: {
+        status: 'awaiting_curation',
+        deleted_dt: null,
+        user_id: req.session.user_id,
+      },
+    }, {model: User, as: 'curator'}],
+    where: {
+      trip_id: req.params.id,
+    },
+  });
+
+  res.locals.trip_id = req.params.id;
+
+  console.log(tripData.get({plain:true}));
+  res.render("trip", {
+    trip: tripData.get({plain:true}), 
+    quote: {
+            ...quoteData.get({plain: true}),
+            formattedAmount:  formatter.format(quoteData.get({plain: true}).amount),
+            viewQuote: true,},
+    viewQuote: true,
+   
+  });
+
+
+});
+
+router.get("/trip/:id/quotes/:quote_id/pay", withAuth, async (req, res) => {
+  const tripData = await Trip.findByPk(req.params.id, {
+    include: [{ model: Destination}],
+    where: {
+      user_id: req.session.user_id,
+    },
+    order:[['destinations', 'order', 'ASC']],
+  });
+
+  const quoteData = await Quote.findByPk(req.params.quote_id, 
+    {
+      include: [{ model: Trip, 
+      where: {
+        status: 'awaiting_curation',
+        deleted_dt: null,
+        user_id: req.session.user_id,
+      },
+    }, {model: User, as: 'curator'}],
+    where: {
+      trip_id: req.params.id,
+    },
+  });
+
+  res.locals.trip_id = req.params.id;
+
+
+  console.log(tripData.get({plain:true}));
+  res.render("payment", {
+    trip: tripData.get({plain:true}), 
+    quote: {
+            ...quoteData.get({plain: true}),
+            formattedAmount:  formatter.format(quoteData.get({plain: true}).amount)
+            },
+  });
+
+
 });
 
 module.exports = router;
